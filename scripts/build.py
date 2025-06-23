22ef2a73261e98d9bf6b000d845fc134f7a17909
yukinoba <ckmagician@gmail.com>
2025-06-23 16:34:57 +0800

    remove unused load_tools_config function and related code from download.py, streamlining the script; clean up build.py by removing unnecessary print statements in compress_directory function

--- scripts/build.py
+++ scripts/build.py
@@ -143,9 +143,7 @@ def compress_directory(root_dir, output_zip, exclude_dirs=None):
     if not file_abs_paths:
         print("沒有檔案需要壓縮！")
         return
-    for file, prefix in zip(file_abs_paths, prefix_rel_paths):
-        print(prefix, file)
-
+    
     try:
         print(f"開始壓縮：{os.path.basename(output_zip)}")
         pyminizip.compress_multiple(file_abs_paths, prefix_rel_paths, output_zip, None, 5)
